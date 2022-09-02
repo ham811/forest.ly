bf7729446d8bfe5f227994c81c28f6f74da7927f
Hamid Amachnoug <69971025+ham811@users.noreply.github.com>
2022-09-02 18:23:16 +0200

    data

--- apps/analytics.py
+++ apps/analytics.py
@@ -11,31 +11,11 @@ from matplotlib.backends.backend_agg import RendererAgg
 def get_data_deputies():
      return pd.read_csv(os.path.join(os.getcwd(),'precip_berlin.csv'))
 
+dataset_url = "https://raw.githubusercontent.com/ham811/forest.ly/main/data/precip_berlin.csv"
 
+# read csv from a URL
+@st.experimental_memo
+def app() -> pd.DataFrame:
+    return pd.read_csv(dataset_url)
 
-def app():
-    #configuration of the page
-    st.set_page_config(layout="wide")
-    #load dataframes
-    df_dep = get_data_deputies()
-    st.title('Patches vizualisation tool')
-    st.markdown("""
-    This app performs simple visualization from the Forest Patches Features!
-    """)
-    st.write(df_dep)
-
-
-    st.sidebar.header('Select what to display')
-    pol_parties = df_dep['Year'].unique().tolist()
-    pol_party_selected = st.sidebar.multiselect('Political parties', pol_parties, pol_parties)
-    nb_deputies = df_dep['Month'].value_counts()
-    nb_mbrs = st.sidebar.slider("Number of members", int(nb_deputies.min()), int(nb_deputies.max()), (int(nb_deputies.min()), int(nb_deputies.max())), 1)
-    #creates masks from the sidebar selection widgets
-    mask_pol_par = df_dep['Month'].isin(pol_party_selected)
-    #get the parties with a number of members in the range of nb_mbrs
-    mask_mbrs = df_dep['Month'].value_counts().between(nb_mbrs[0], nb_mbrs[1]).to_frame()
-    mask_mbrs= mask_mbrs[mask_mbrs['Month'] == 1].index.to_list()
-    mask_mbrs= df_dep['Month'].isin(mask_mbrs)
-
-    df_dep_filtered = df_dep[mask_pol_par & mask_mbrs]
-    st.write(df_dep_filtered)   
+df = app()
