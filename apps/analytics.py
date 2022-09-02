7d895d537620766855a612ea5b0a5ecde583a764
Hamid Amachnoug <69971025+ham811@users.noreply.github.com>
2022-09-02 18:04:47 +0200

    analytics

--- apps/analytics.py
+++ apps/analytics.py
@@ -1,15 +1,42 @@
-import streamlit as st
-import leafmap.foliumap as leafmap
+import streamlit as st 
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from matplotlib.backends.backend_agg import RendererAgg
+
+
+#Loading the data
+@st.cache
+def get_data_deputies():
+     return pd.read_csv(os.path.join(os.getcwd(),'precip_berlin.csv'))
+
+
 
 def app():
+    #configuration of the page
+    st.set_page_config(layout="wide")
+    #load dataframes
+    df_dep = get_data_deputies()
+    st.title('Patches vizualisation tool')
+    st.markdown("""
+    This app performs simple visualization from the Forest Patches Features!
+    """)
+    st.write(df_dep)
 
-    st.title("Analyitics")
-    with st.expander("See source code"):
-        with st.echo():
-            m = leafmap.Map()
-            m.split_map(
-                left_layer='Global Aboveground and Belowground Biomass Carbon Density Maps', right_layer='ESA WorldCover 2020'
-            )
-            m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
 
-    m.to_streamlit(height=700)
+    st.sidebar.header('Select what to display')
+    pol_parties = df_dep['Year'].unique().tolist()
+    pol_party_selected = st.sidebar.multiselect('Political parties', pol_parties, pol_parties)
+    nb_deputies = df_dep['Month'].value_counts()
+    nb_mbrs = st.sidebar.slider("Number of members", int(nb_deputies.min()), int(nb_deputies.max()), (int(nb_deputies.min()), int(nb_deputies.max())), 1)
+    #creates masks from the sidebar selection widgets
+    mask_pol_par = df_dep['Month'].isin(pol_party_selected)
+    #get the parties with a number of members in the range of nb_mbrs
+    mask_mbrs = df_dep['Month'].value_counts().between(nb_mbrs[0], nb_mbrs[1]).to_frame()
+    mask_mbrs= mask_mbrs[mask_mbrs['Month'] == 1].index.to_list()
+    mask_mbrs= df_dep['Month'].isin(mask_mbrs)
+
+    df_dep_filtered = df_dep[mask_pol_par & mask_mbrs]
+    st.write(df_dep_filtered)   
