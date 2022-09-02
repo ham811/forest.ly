3924e31f5084cdd0f1954ba743c1250579cabbcc
Hamid Amachnoug <69971025+ham811@users.noreply.github.com>
2022-09-02 15:09:06 +0200

    ESA LandCover

--- apps/split.py
+++ apps/split.py
@@ -1,17 +1,15 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
-st.set_page_config(layout="wide")
+def app():
 
+    st.title("ESA LandCover")
+    with st.expander("See source code"):
+        with st.echo():
+            m = leafmap.Map()
+            m.split_map(
+                left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
+            )
+            m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
 
-st.title("Split-panel Map")
-
-with st.expander("See source code"):
-    with st.echo():
-        m = leafmap.Map()
-        m.split_map(
-            left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
-        )
-        m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
-
-m.to_streamlit(height=700)
+    m.to_streamlit(height=700)
