2ff2c9ebc44a29b85fbbd59774eab9c035a7c5e7
Hamid Amachnoug <69971025+ham811@users.noreply.github.com>
2022-09-02 15:05:01 +0200

    split_map

---
+++ apps/split.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+
+st.title("Split-panel Map")
+
+with st.expander("See source code"):
+    with st.echo():
+        m = leafmap.Map()
+        m.split_map(
+            left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
+        )
+        m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
+
+m.to_streamlit(height=700)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import home, heatmap, upload, app # import your app modules here
+from apps import home, heatmap, upload, app, split # import your app modules here
 
 st.set_page_config(page_title="GeoIT App", layout="wide")
 
@@ -11,6 +11,7 @@ apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
     {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
     {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+    {"func": split.app, "title": "ESA WorldCover", "icon": "map"},
 ]
 
 titles = [app["title"] for app in apps]
